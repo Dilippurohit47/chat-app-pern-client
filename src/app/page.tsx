230007876b9b7e4676708a27e673a68b04d2703a
Dilippurohit47 <dilippurohit204@gmail.com>
2024-12-01 19:44:54 +0530

    all components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-import { Button } from "@/components/ui/button";
+import Navbar from "@/components/base/Navbar";
+import HeroSection from "@/components/base/HeroSection";
+import FeatureSection from "@/components/base/FeatureSection";
+import UserReviews from "@/components/base/UserReviews";
+import Footer from "@/components/base/Footer";
+// import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
+// import { getServerSession } from "next-auth";
+export default async function LandingPage() {
+  // const session: CustomSession | null = await getServerSession(authOptions);
+  return (
+    <div className="min-h-screen flex flex-col ">
+      {/* Header */}
+      <Navbar/>
+      {/* Hero Section */}
+      <HeroSection />
 
-export default function Home() {
-  return <div>
+      {/* Features Section */}
+      <FeatureSection />
 
-  </div>
+      {/* User Reviews Section */}
+      <UserReviews />
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 }
--- src/components/base/Navbar.tsx
+++ src/components/base/Navbar.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import { CustomUser } from "@/app/api/auth/[...nextauth]/options";
 import { Button } from "../ui/button";
-import LoginModal from "../auth/LoginModal";
-export default function Navbar({ user }: { user: CustomUser | null }) {
+export default function Navbar() {
+
+  const user  = false
   return (
     <nav className="p-6 flex justify-between items-center bg-white shadow-sm">
       <h1 className="text-xl md:text-2xl font-extrabold">QuickChat</h1>
@@ -12,7 +12,9 @@ export default function Navbar({ user }: { user: CustomUser | null }) {
         <Link href="/">Home</Link>
         <Link href="#features">Features</Link>
         {!user ? (
-          <LoginModal />
+          <div>
+            Loginmodal import here
+          </div>
         ) : (
           <Link href="/dashboard">
             <Button>Dashboard</Button>
